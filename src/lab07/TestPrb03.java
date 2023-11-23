4d98bbea81eb0db63eebb17bca3f4147eeaac403
Coșa Andrei Valeriu <42703353+AndreiOp235@users.noreply.github.com>
2023-11-23 23:53:13 +0200

    added functionality for the last row of buttons

--- src/lab07/TestPrb03.java
+++ src/lab07/TestPrb03.java
@@ -8,27 +8,23 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class TestPrb03 {
-
+	static String caleEXEC="";
 	public static void main(String[] args) {
-<<<<<<< Updated upstream
-		String argumente[]= {"7","8","9","del","4","5","6","÷","1","2","3","*","0","+","-","="};
-=======
 		String argumente[]= {"7","8","9","del","4","5","6","÷","1","2","3","*","+","0","-","CLR","(",".",")","="};
->>>>>>> Stashed changes
 		// TODO Auto-generated method stub
 		System.out.println("Neata !");
 		JFrame gui= new JFrame("Calculator");
 		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		gui.setSize(395,520);
+		gui.setSize(395,610);
 		ZonaText z=new ZonaText();
 		
 		gui.getContentPane().setBackground(Color.cyan);
 		gui.setLayout(null);
 		apasareButon buttonListener = new apasareButon(z);
 		
-		Butoane b[]=new Butoane[16];
+		Butoane b[]=new Butoane[20];
 		
-		for(int i=1;i<=16;i++)
+		for(int i=1;i<=20;i++)
 		{
 			b[i-1]=new Butoane(argumente[i-1],10+((i-1)%4)*90,115+((i-1)/4)*90);
 			gui.add(b[i-1]);
